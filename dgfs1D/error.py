9a22b8897aad47c878b4769ba919b3b94d335316
Shashank Jaiswal <jaisw7@gmail.com>
2025-02-23 22:37:16 -0500

    Only compute errors on distributions

--- dgfs1D/error.py
+++ dgfs1D/error.py
@@ -1,27 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from argparse import ArgumentParser, FileType
+from argparse import ArgumentParser
 from dgfs1D.quadratures import zwglj, nodal_basis_at
-import re
 import numpy as np
 import h5py
-
-def getNeNq(file):
-    m = re.search(r"(s=\d+)\.(k=\d+)", file)
-    if not m: raise ValueError("Improper filename")
-    return int(m.group(1).split("=")[-1]), int(m.group(2).split("=")[-1])
+from pathlib import Path
 
 def decode(file):
-    Ne, Nq = getNeNq(file)
-    try:
-        with h5py.File(file, 'r') as h5f:
-            dst = h5f['coeff']
-            iK, iNe, iNv = dst.attrs['K'], dst.attrs['Ne'], dst.attrs['Nv']
-            assert iK==Nq, "Inconsistent distribution K"
-            assert iNe==Ne, "Inconsistent distribution Ne"
-            data = np.array(dst[:]).reshape((Nq, Ne, -1))
-    except:
-        data = np.loadtxt(file, comments="#").reshape((Ne, Nq, -1)).swapaxes(0,1)
+    with h5py.File(file, 'r') as h5f:
+        dst = h5f['coeff']
+        Nq, Ne, Nv = dst.attrs['K'], dst.attrs['Ne'], dst.attrs['Nv']
+        data = np.array(dst[:]).reshape((Nq, Ne, Nv))
     return Ne, Nq, data
 
 def compare(Nq, f, f2):
@@ -37,11 +26,7 @@ def compare(Nq, f, f2):
     diff = np.abs(fr - f2r); # L1
 
     L1e = 0.5*np.einsum('q,qjk->jk', w, diff)
-
-    if nvars>32:
-        return [0, np.sum(L1e, axis=(0, 1))/Ne2/nvars]
-    else:
-        return np.sum(L1e, axis=0)
+    return np.sum(L1e, axis=(0, 1))/Ne2/nvars
 
 def compute_error(file1, file2):
     # Read in the solutions
@@ -56,8 +41,8 @@ def compute_error(file1, file2):
 
 def __main__():
     ap = ArgumentParser()
-    ap.add_argument('file1', type=FileType('r'), help='input file')
-    ap.add_argument('file2', type=FileType('r'), help='input file')
+    ap.add_argument('file1', type=Path, help='input file')
+    ap.add_argument('file2', type=Path, help='input file')
     args = ap.parse_args()
+    assert args.file1.exists() and args.file2.exists(), "Input files not found"
     return compute_error(args.file1, args.file2)
-
